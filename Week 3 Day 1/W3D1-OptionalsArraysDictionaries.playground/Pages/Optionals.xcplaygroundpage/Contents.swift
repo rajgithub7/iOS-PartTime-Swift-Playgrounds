8242395d4b390b3286733650d07a37ff6241b193
Cory Alder <coryalder@gmail.com>
2016-03-08 16:53:44 -0800

    half of W3D1 rewrite

--- Week 3 Day 1/W3D1-OptionalsArraysDictionaries.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ Week 3 Day 1/W3D1-OptionalsArraysDictionaries.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -4,42 +4,58 @@ import UIKit
 //:
 //: ## Optionals and nil
 //:
-//: So far we've somewhat glossed over this topic by avoiding optionals, but often they can't be avoided.
+//: So far we've glossed over this topic by avoiding optionals. That's good! We try to avoid optionality, because it simplifies our code. But often optionals can't be avoided.
 //:
-//: Say we have a string but the string is actually a number. We want to add 1 to it, so we need to convert it to an Int. But we can run into some complications...
+//: What follows is an explaination of an unavoidable optional.
+//:
+//: If we have a Double and an Int, and we want to do math with them, we have to convert one of them. We can't add two different types.
+
+var ratio: Double = 10.0
+var count: Int = 20
+
+// ratio * count // this line does not compile.
+
+//: To get around this we can convert between types. Creating a new Int or a Double using a Double or an Int as the starting type.
+
+var countDouble = Double(count)
+
+ratio * countDouble // works!
+
+//:
+//: With that kind of conversion in mind, let's say we have a string that contains the characters 1 and 0, e.g. "10". That looks like a number, but it's a string that contains a text representation of a number.
+//:
+//: If we want to add 1 to our number-containing-string, we need to convert it to a number. We can do that!
 
 var numberString = "10"
+var convertedNum = Double(numberString)
 
-//: We can convert some types like this:
+//: Now look at the type of the variable `convertedNum`. It's not actually a `Double` it's a `Double?`. The question mark at the end of the type indicates that this variable is "optional". That means we might have a double or we might have nothing. This is because, unlike converting an Int to a Double, the conversion between a `String` and a `Double` might fail! It could fail because some strings don't make sense as numbers. How would you convert the string "😎" to a `Double`...?
+//:
+//: Ok so that's easy to understand! Things that can fail return optionals (`Int?`), things that can't fail give us back non-optionals (`Int`). Cool!
+//:
+//: So now let's try to add 1 to our `convertedNum` variable.
 
-var intVersion = 10
-var doubleVersion = Double(intVersion)
+//convertedNum = convertedNum + 1 // uncomment this line!
 
-//: Let's add 1 to doubleVersion
+//: Looks like the compiler didn't like that! It's giving us a cryptic error about applying binary operators and types and things, and offers the suggestion of adding an exclaimation point!
+//:
+//: We aren't able to do math with `convertedNum` because while it's easy to say how to add `convertedNum` and `1` together, Swift doesn't know how to add maybe-nothing-maybe-something and `1` together.
+//:
+//: Adding an exclaimation point is called "force unwrapping". It will make the code work if `convertedNum` contains a number, but it will crash the Playground if our `numberString` contains something that can't be converted to a number.
 
-doubleVersion += 1
-
-//: Well it turns out we can do the same with converting a string to a number:
-
-//: #### Challenge 1
-//: Convert numberString to an int.
-
-
-
-
-// Now try increment this int by 1 by uncommenting the line below
-
-//convertedInt += 1
-
-//: You may notice you run into an error. Whats the different? Use option click to see what type doubleVersion and convertedInt are. doubleVersion is a Double but convertedInt is an Int? (Int Optional). This is because not all strings can be converted to numbers ex) "hello" cannot be converted to a number but "10" can. Below, try converting the string "hello" to an int. What happens?
+//: ## Challenge 1
+//:
+//: Add 1 to convertedNum using a force-unwrap (!).
+//: Now let's do the same steps again, converting a string to an Int and then adding 1 to it by force unwrapping, but this time our string is "hello" not "10".
 
 var maybeInt = "hello"
 var myNewInt = Int(maybeInt)
 
-//: Trying to convert a string to a Int, which cannot be converted to an Int, results in returing a value of nil.
-//: Note: myNewInt is still of "Optional Int" type written `Int?`
-//: There are a few ways to deal with optionals in swift, and most involve "unwrapping" them.
+// add 1 to myNewInt here:
 
+
+//: ## Challenge 2
+//:
 //: In the below if, we are "unwrapping" myNewInt, ie) unwrapped will only exist if myNewInt is not nil.
 //: Moreover, if you option click on the variable unwrapped you will notice that it is of type Int not Int?
 
